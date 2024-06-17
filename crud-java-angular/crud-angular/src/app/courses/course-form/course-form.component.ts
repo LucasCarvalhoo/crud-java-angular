500ec8c4f93af57fe3e11e7acf8dde0983e290dc
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2024-06-17 18:30:15 -0300

    angular type form

--- crud-java-angular/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-java-angular/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,9 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { CoursesService } from '../services/courses.service';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Location } from '@angular/common'
 
+import { CoursesService } from '../services/courses.service';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
@@ -11,39 +11,32 @@ import { Location } from '@angular/common'
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup; // faz referencia a tag form
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location
-  ) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    })
-
-
+    private location: Location) {
+    //this.form
   }
 
   ngOnInit(): void {
   }
-
-  onSubmit(){
-    this.service.save(this.form.value).subscribe(result => this.onSuccess(), error => this.onError());
+  onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
   }
-
-  onCancel(){
+  onCancel() {
     this.location.back();
   }
-
-  private onSuccess(){
-    this.snackBar.open('Curso salvo com sucesso!', '', {duration: 5000})
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
     this.onCancel();
   }
-
-  private onError(){
-    this.snackBar.open('Erro ao salvar', '', {duration: 5000})
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 });
   }
-
 }
