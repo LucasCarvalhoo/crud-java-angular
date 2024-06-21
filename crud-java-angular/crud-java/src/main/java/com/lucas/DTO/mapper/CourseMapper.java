b7627c82a175e66d7549af648d601334c224bf1a
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2024-06-21 08:35:49 -0300

    mapper

---
+++ crud-java-angular/crud-java/src/main/java/com/lucas/DTO/mapper/CourseMapper.java
@@ -0,0 +1,28 @@
+package com.lucas.DTO.mapper;
+
+import com.lucas.DTO.CourseDTO;
+import com.lucas.model.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+    public CourseDTO toDTO(Course course){
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+    }
+
+    public Course toEntity(CourseDTO courseDTO){
+        if(courseDTO == null){
+            return null;
+        }
+
+        Course course = new Course();
+        if(courseDTO.id() != null){
+            course.setId(courseDTO.id());
+        }
+        //no dto não tem alteração
+        course.setName(courseDTO.name());
+        course.setCategory(courseDTO.category());
+        course.setStatus("Ativo");
+        return course;
+    }
+}
--- crud-java-angular/crud-java/src/main/java/com/lucas/service/CourseService.java
+++ crud-java-angular/crud-java/src/main/java/com/lucas/service/CourseService.java
@@ -1,7 +1,8 @@
 package com.lucas.service;
 
+import com.lucas.DTO.CourseDTO;
+import com.lucas.DTO.mapper.CourseMapper;
 import com.lucas.exception.RecordNotFoundException;
-import com.lucas.model.Course;
 import com.lucas.repository.CourseRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Validated
@@ -20,31 +22,37 @@ import java.util.List;
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, CourseMapper courseMapper) {
         this.courseRepository = courseRepository;
+        this.courseMapper = courseMapper;
     }
 
-    public List<Course> list() {
-        return courseRepository.findAll();
+    public List<CourseDTO> list() {
+        return courseRepository.findAll()
+                .stream()
+                .map(courseMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     // Optional -> maneira de transmitir que o tipo de retorno pode ou não haver um valor, sem usar null
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id).map(courseMapper::toDTO)
+                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public Course create(@RequestBody @Valid Course course) {
-        return courseRepository.save(course);
+    public CourseDTO create(@Valid @NotNull CourseDTO course) {
+        return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
     }
 
-    public Course update(@PathVariable @NotNull @Positive Long id,
-                                   @RequestBody @Valid Course course) {
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id,
+                                   @RequestBody @Valid CourseDTO course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    return courseRepository.save(recordFound);
+                    recordFound.setName(course.name());
+                    recordFound.setCategory(course.category());
+                    return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
                 }).orElseThrow(() -> new RecordNotFoundException(id));
         }
 
